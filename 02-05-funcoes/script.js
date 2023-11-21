6e3a1c551f1fd8b3fedf54e0c3f5df85e8013f13
victorfreireavfs <victoravfs@gmail.com>
2023-11-20 22:00:39 -0300

    funções e condicionais

--- 02-05-funcoes/script.js
+++ 02-05-funcoes/script.js
@@ -1,8 +1,18 @@
-function greet (name){
-    console.log('boa madrugada, ' + name)
+/*function greet (name){
+    return 'boa madrugada, ' + name
 }
 
-greet('Victor') 
+/*greet('Victor') 
 greet('Belma')
 greet('Marvson') 
 
+var greeting = greet('Victor')
+
+console.log(greeting)*/
+
+function falar(name){
+    return 'olá! meu nome é ' + name
+}
+
+var falando = falar('Clara')
+console.log(falando)
---
+++ 02-06-condicionais/script.js
@@ -0,0 +1,16 @@
+var eu = {
+    nome : 'Victor',
+    idade : 55
+}
+
+    function checaridade(pessoa){
+        console.log('A idade da pessoa é: ' + pessoa.idade)
+        if(eu.idade < 18){
+            console.log('esta pessoa é menor de idade!')
+        }else{
+            console.log('esta pessoa é maior de idade')
+        }
+
+    }
+
+    checaridade(eu)
