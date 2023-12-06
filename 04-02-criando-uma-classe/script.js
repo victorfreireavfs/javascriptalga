a5f4e77105fb3d1179a89e75b18af6859ea7ad64
victorfreireavfs <victoravfs@gmail.com>
2023-12-05 21:25:10 -0300

    update

--- 04-02-criando-uma-classe/script.js
+++ 04-02-criando-uma-classe/script.js
@@ -1,3 +1,13 @@
-class humano{
-    
+class Humano{
+    fome = true
+    comer (){
+        this.fome = false
+    }
 }
+
+const person = new Humano()
+const person2 = new Humano()
+
+
+console.log(person)
+console.log(person2)
